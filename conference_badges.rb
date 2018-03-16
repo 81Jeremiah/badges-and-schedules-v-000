d79871c7fde36f047e9d7279c0232ba89feb1b7f
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-16 22:25:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,10 @@
 # Write your code here.
 #badge maker
-def badge_maker(names)
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
   badges = []
   names.each{|name| badges << "Hello, my name is #{name}."}
   badges
